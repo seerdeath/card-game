fd192919f5ec29e55af8a9ccaca92e49939f4e10
seerdeath <artemengel19@gmail.com>
2026-01-21 12:45:34 +0500

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,1 +1,29 @@
+const socket = io();
+let roomId = null;
 
+function createRoom() {
+  const name = document.getElementById("name").value;
+  socket.emit("create_room", name);
+}
+
+socket.on("room_joined", (room) => {
+  roomId = room.id;
+  render(room);
+});
+
+socket.on("room_update", (room) => {
+  render(room);
+});
+
+function draw() {
+  socket.emit("draw_card", roomId);
+}
+
+function render(room) {
+  document.getElementById("game").innerHTML = `
+    <h2>Комната: ${room.id}</h2>
+    <ul>${room.players.map(p => `<li>${p.name}: ${p.score}</li>`).join("")}</ul>
+    <button onclick="draw()">Вытянуть карту</button>
+    <pre>${room.log.join("\n")}</pre>
+  `;
+}
