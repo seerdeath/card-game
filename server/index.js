7d87d7c25bf1383f0e726500e6c8d0444d146016
seerdeath <artemengel19@gmail.com>
2026-01-21 12:42:34 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,32 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { createRoom, joinRoom, drawCard } from "./rooms.js";
 
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.static("client"));
+
+io.on("connection", (socket) => {
+  socket.on("create_room", (name) => {
+    const room = createRoom(socket.id, name);
+    socket.join(room.id);
+    socket.emit("room_joined", room);
+  });
+
+  socket.on("join_room", ({ roomId, name }) => {
+    const room = joinRoom(roomId, socket.id, name);
+    socket.join(room.id);
+    io.to(room.id).emit("room_update", room);
+  });
+
+  socket.on("draw_card", (roomId) => {
+    const room = drawCard(roomId, socket.id);
+    io.to(room.id).emit("room_update", room);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
