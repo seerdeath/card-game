b4de728e6b05e4157a22a7d49dcd57c80e788932
seerdeath <artemengel19@gmail.com>
2026-01-21 13:01:04 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,6 +41,9 @@ io.on("connection", socket => {
   });
 });
 
-httpServer.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
 });
